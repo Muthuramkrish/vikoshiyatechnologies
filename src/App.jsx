a8885c4facab02057cc60f92b9a35b874eddda30
Muthuramakrishnan <vikoshiya.muthuramakrishnan@gmail.com>
2025-08-01 10:57:52 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,28 @@ import WebDevelopment from './pages/WebDevelopment.jsx'
 import QualityAssurance from './pages/QualityAssurance.jsx'
 import BlogPost from './pages/BlogPost.jsx'
 import Ecommerce from './pages/Ecommerce.jsx'
+import UnderMaintenance from './pages/UnderMaintenance.jsx'
 
 function App() {
   return (
-    <div className="font-sans bg-gray-50">
-      <ScrollToTop />
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/web-development" element={<WebDevelopment />} />
-        <Route path="/quality-assurance" element={<QualityAssurance />} />
-        <Route path="/blog/:id" element={<BlogPost />} />
-        <Route path="/ecommerce" element={<Ecommerce />} />
-      </Routes>
-    </div>
+    // <div className="font-sans bg-gray-50">
+    //   <ScrollToTop />
+    //   <Navigation />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/blog" element={<Blog />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //     <Route path="/privacy" element={<Privacy />} />
+    //     <Route path="/terms" element={<Terms />} />
+    //     <Route path="/web-development" element={<WebDevelopment />} />
+    //     <Route path="/quality-assurance" element={<QualityAssurance />} />
+    //     <Route path="/blog/:id" element={<BlogPost />} />
+    //     <Route path="/ecommerce" element={<Ecommerce />} />
+    //     <Route path="/under-maintenance" element={<UnderMaintenance />} />
+    //   </Routes>
+    // </div>
+    <UnderMaintenance />
   )
 }
 
